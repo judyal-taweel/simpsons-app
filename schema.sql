a89a7c5c038bcdf474c532a651472707c0b24081
judyal-taweel <judybataineh@gmail.com>
2021-04-20 12:34:26 +0300

    final lab

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS final;
+CREATE TABLE final(
+id SERIAL PRIMARY KEY,
+characters VARCHAR(255),
+quote VARCHAR(255),
+image VARCHAR(255),
+characterDirection VARCHAR(255)
+
+);
--- server.js
+++ server.js
@@ -16,24 +16,88 @@ const PORT = process.env.PORT || 3000;
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
 
+app.use(express, urlencoded({extended:true}));
 // Specify a directory for static resources
 
+app.use(express.static('./public'));
 // define our method-override reference
+app.use(methodOverride ,('_method'));
 
 // Set the view engine for server-side templating
-
+app.use(view , 'ejs')
 // Use app cors
 
-
+app.use(express ,'cors');
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
+app.get('/' ,indexhandle);
+app.get('/favorite-quotes' ,getfav);
+app.get('/favorite-quotes/:id' ,detailspage);
+app.post('/favorite-quotes' ,favpage);
+app.put('/favorite-quotes/:id' ,updatedata);
+app.delete('/favorite-quotes/:id' ,deletedata);
+
+
 
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
+
+function indexhandle(req,res){
+    let url = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=10'
+
+    superagent.get(url).set('User-Agent', '1.0').then(data =>{
+        res.render('pages/index' ,{data:data.body});
+    })
+    
+}
+
+function getfav(req,res){
+    let sql = 'SELECT * FROM final;';
+    client.query(sql).then(data =>{
+        res.render('pages/fav' ,{data:data.rows});
+    })
+}
+
+function favpage(req,res){
+    let sql ='INSERT INTO final (characters,quote,image) VALUES ($1,$2,$3);';
+    let values = [req.body.character,req.body.quote,req.body.image];
+    client.query(sql,values).then(data =>{
+        res.redirect('/favorite-quotes');
+    })
+}
+
+function detailspage(req,res){
+    let id = req.params.id;
+    let sql = 'SELECT * FROM final WHERE id=$1;';
+    client.query(sql,[id]).then(result =>{
+        res.render('pages/details' ,{results:result.rows});
+    })
+}
+
+function updatedata(req,res){
+    let id = req.params.id;
+    let sql = 'UPDATE final SET characters=$1,quote=$2,image=$3 WHERE id=$4;';
+    let values = [req.body.character,req.body.quote,req.body.image,id];
+
+    client.query(sql,values).then(result =>{
+        res.redirect(`/details/${id}`);
+    })
+}
+
+function deletedata(req,res){
+    let id = req.params.id;
+    let sql = 'DELETE FROM final WHERE id=$1;';
+
+    client.query(sql,[id]).then(result =>{
+        res.redirect('/');
+    })
+
+}
+
 
 // helper functions
 
